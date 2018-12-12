66d327ac5bf554e706c15b5e95974613f5fdc490
Abul Hasnat Nayeem <nayeem2693@gmail.com>
2018-12-12 18:49:55 +0100

    Add profile model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=45, blank=True)
+    last_name = models.CharField(max_length=45, blank=True)
+    phone = models.CharField(max_length=45, blank=True)
+    email = models.EmailField(blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=100, default='Bangladesh', blank=True)
+    token = models.CharField(max_length=255, blank=True)
+

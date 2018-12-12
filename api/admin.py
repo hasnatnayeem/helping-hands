bbad85743ac52996c332ec58f3285618e687365e
Abul Hasnat Nayeem <nayeem2693@gmail.com>
2018-12-12 19:00:53 +0100

    Customize admin for Profile model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ('user', 'first_name', 'last_name', 'phone', 'email', 'address', )
+    list_display = ('first_name', 'last_name', 'phone', 'email', 'address', )
+    list_filter = ['first_name']
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(ProfileAdmin, self).get_form(request, obj, **kwargs)
+        # Show only unassinged usernames as options while creating profile
+        if not obj:
+            form.base_fields['user'].queryset = User.objects.filter(profile__isnull=True)
+        return form

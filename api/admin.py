4b34dd9c9546b6ab6a5f743d7b897dcebac01a1e
Abul Hasnat Nayeem <nayeem2693@gmail.com>
2018-12-12 19:50:34 +0100

    Customize admin for donation

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Profile
+from .models import Donation
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     fields = ('user', 'first_name', 'last_name', 'phone', 'email', 'address', )
     list_display = ('first_name', 'last_name', 'phone', 'email', 'address', )
-    list_filter = ['first_name']
+    list_filter = []
 
     def get_form(self, request, obj=None, **kwargs):
         form = super(ProfileAdmin, self).get_form(request, obj, **kwargs)
@@ -14,3 +15,18 @@ class ProfileAdmin(admin.ModelAdmin):
         if not obj:
             form.base_fields['user'].queryset = User.objects.filter(profile__isnull=True)
         return form
+
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    # fields = ('donor', 'collector', 'collected_at', )
+    # list_display = ('donor', 'collector', 'collected_at', )
+    # list_filter = ['collected_at']
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(DonationAdmin, self).get_form(request, obj, **kwargs)
+        # Show only unassinged usernames as options while creating profile
+        if not obj:
+            form.base_fields['collector'].queryset = User.objects.filter(is_staff=True, is_superuser=False)
+        return form
+    # pass

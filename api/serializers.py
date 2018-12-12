8f9de2fda4ad81a3611f6622b6c18d4ae88da63a
Abul Hasnat Nayeem <nayeem2693@gmail.com>
2018-12-12 22:16:32 +0100

    Format donation date in serializer

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,8 @@ class DonationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donation
         fields = ('id', 'donor_name' ,'amount', 'collector_name', 'collected_at')
+
+    def to_representation(self, instance):
+        representation = super(DonationSerializer, self).to_representation(instance)
+        representation['collected_at'] = instance.collected_at.strftime('%d-%m-%Y')
+        return representation
